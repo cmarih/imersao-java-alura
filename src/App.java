44ec3c4882bc5a005d88beeb00a92e309b950218
cmarih <marycostajc@gmail.com>
2022-07-18 22:55:05 -0300

    Criando conexão com a API do IMDB para buscar os filmes melhores ranqueados.

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.rmi.server.ExportException;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Fazer uma conexão HTTP e buscar os top 250 filmes
+        String url = "https://api.mocki.io/v2/549a5d8b";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String body = response.body();
+
+        System.out.println(body);
+        //Extrair dados: Titulo, post e classificação
+
+        //Exibir e manipular os dados na aplicação
+    }
+}
