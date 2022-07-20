698e946a75e72b3769ea62add2e09b0dfc83d03e
cmarih <marycostajc@gmail.com>
2022-07-19 21:16:42 -0300

    Finalizado desafio de utilização de emojis.

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.io.PrintStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -24,18 +25,43 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
         //Retorno da lista de filmes
-        System.out.println(listaDeFilmes.size());
-        System.out.println(listaDeFilmes.get(0));
+        /*System.out.println(listaDeFilmes.size());
+        System.out.println(listaDeFilmes.get(0));*/
 
         //Exibir e manipular os dados na aplicação
 
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
+            var titulo = filme.get("title");
+            var nota = filme.get("imDbRating");
+            var imagem = filme.get("image");
 
-            System.out.println(filme.get("imDbRating"));
+            System.out.println(" \u001b[5m\u001b[30m\u001b[43m" + " Titulo: " + titulo + " \u001b[m ");
+            System.out.println(" \u001b[1m" + " Poster: " + imagem + "\u001b[m");
+            System.out.println(" \u001b[38;5;226m\u001b[48;5;53m Classificação: " + nota + " \u001b[m ");
+
+            var rating = Double.parseDouble(nota);
+            //System.out.println(nota);
+                    if(rating >= 9) {
+
+                       int numeroEstrelas = 1 ;
+
+                       while(numeroEstrelas <= 5){
+                           System.out.print(" \u001b[5m\u001b[33m\u2b50 ");
+                            numeroEstrelas ++;
+                       }
+                        System.out.println();
+                   } else {
+                        int numeroEstrelas = 1 ;
+
+                        while(numeroEstrelas <= 4){
+                            System.out.print(" \u001b[5m\u001b[33m\u2b50\u001b[m ");
+                            numeroEstrelas ++;
+                        }
+                        System.out.println();
+                    }
 
             System.out.println();
+
         }
     }
 }
